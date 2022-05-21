cbf40820aca6783182c47b0f36e8f06551168326
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-05-21 16:44:04 -0300

    feat: adicionado UserController

---
+++ src/livraria.net.api/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using livraria.net.api.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace livraria.net.api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public UserDTO create(UserDTO userDTO)
+        {
+            return userDTO;
+        }
+
+        [HttpGet("{id}")]
+        public UserDTO FindById(int id)
+        {
+            return new UserDTO { Id = id };
+        }
+
+        [HttpGet]
+        public List<UserDTO> FindAll()
+        {
+            return new List<UserDTO>();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var userDTO = new UserDTO { Id = id };
+        }
+
+        [HttpPut("{id}")]
+        public UserDTO Update(int id, UserDTO userDTO)
+        {
+            return userDTO;
+        }
+    }
+}
